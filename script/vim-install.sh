343d909137227834a1b9d59af18a98a4522bcbe1
tt <ttc@zju.edu.cn>
2020-12-05 17:22:39 +0800

    feat: vim install script

---
+++ script/vim-install.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+#git clone https://github.com/vim/vim.git ~/vim
+mkdir ~/vim82
+mkdir ~/vim82/etc
+mkdir ~/vim82/usr
+cd ~/vim/src
+./configure --prefix=$HOME/vim82/usr
+make VIMRCLOC=$HOME/vim82/etc VIMRUNTIMEDIR=$HOME/vim82/usr/share/vim/vim82 MAKE="make -e"
+make install
+
+
